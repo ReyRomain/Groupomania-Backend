079da490287e86fe9e79a2e6ab71ce8c94962bbd
Romain Rey <romainrey.contact@gmail.com>
2022-06-01 23:35:16 +0200

    feat: Ajout du middleware auth

---
+++ middlewares/auth.js
@@ -0,0 +1,31 @@
+/**
+ * @typedef  {import('express').Request}      IncomingMessage
+ * @typedef  {import('express').Response}     ServerResponse
+ * @typedef  {import('express').NextFunction} NextFunction
+*/
+
+const jwt = require('jsonwebtoken');
+
+/**
+ * Middleware d'authentification
+ *
+ * @param   {IncomingMessage}   req   récupère la requête d'authentification
+ * @param   {ServerResponse}    res   la réponse
+ * @param   {NextFunction}      next  passe à la fonction suivante
+ *
+ * @return  {void}
+ */
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.JWT_PASS);
+        // @ts-ignore
+        if (req.body.userId && req.body.userId !== decodedToken.userId) throw 'Invalid user ID';
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            error
+        });
+    }
+};
