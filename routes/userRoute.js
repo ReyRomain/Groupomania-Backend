0234b1f9025c7dce084730a91734b6757eedf904
Romain Rey <romainrey.contact@gmail.com>
2022-06-05 23:26:06 +0200

    feat: Ajout de route pour user

---
+++ routes/userRoute.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+/**
+ * Middleware controller
+ */
+const {login, signup} = require("../controllers/userCtrl");
+
+/**
+ * Middleware auth
+ */
+const auth = require("../middlewares/auth");
+
+const router = express.Router();
+ 
+router.post('/signup', signup);
+router.post('/login', login);
+ 
+module.exports = router;
