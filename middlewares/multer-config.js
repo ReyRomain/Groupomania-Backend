6e39807e0af8b7b54adafc43aec2387c0fe6a7c0
Romain Rey <romainrey.contact@gmail.com>
2022-06-05 23:33:10 +0200

    feat: Ajout de multer

---
+++ middlewares/multer-config.js
@@ -0,0 +1,48 @@
+/**
+ * Package Multer
+*/
+const multer = require('multer');
+const maxSize = 1 * 1000 * 1000; //1Mo
+
+/**
+ * Dictionnaire pour générer l'extension du fichier
+ *
+ * @type {Object}
+ */
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
+};
+
+/**
+ * Explique à multer où enregistrer les fichiers
+ */
+const destination = (req, file, callback) => {
+   callback(null, 'images');
+}
+ 
+/**
+ * Explique à multer quel nom de fichier utiliser
+ */
+const filename = (req, file, callback) => {
+
+    /**
+     * On utilise le nom d'origine du fichier et on remplace les espaces par underscore
+     */
+    const name = file.originalname.split(' ').join('_');
+
+    /**
+     * Création de l'extension de fichier
+     */
+    const extension = MIME_TYPES[file.mimetype];
+    /**
+     * Création du fileName entier
+     */
+    callback(null, name + Date.now() + '.' + extension);
+};
+
+module.exports = multer({
+    storage: multer.diskStorage({ destination, filename }),  
+    limits: { fileSize: maxSize }
+}).single('image');
