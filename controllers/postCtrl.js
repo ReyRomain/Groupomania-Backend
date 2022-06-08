8c45fa2548e26636ccedde57e62e2847d8847b36
Romain Rey <romainrey.contact@gmail.com>
2022-06-08 13:23:56 +0200

    feat: Ajout de getAllPosts & getPostByUser

--- controllers/postCtrl.js
+++ controllers/postCtrl.js
@@ -31,5 +31,37 @@ function getAllPosts2(req, res, next) {
     db.exec(sqlGetAllPosts, function (error, result){
         if (error) res.status(400).json({ error });
         res.status(200).json(result)
-    })
+    });
+}
+
+
+
+
+/**
+ * Récupération d'un post
+ *
+ * @param   {IncomingMessage}   req    la requête complétée
+ * @param   {ServerResponse}    res    la réponse
+ * @param   {NextFunction}      next   passe à la fonction suivante
+ *
+ * @return  {void}                     retourne le post selectionné & envoie une réponse
+ */
+function getPostByUser (req, res, next) {
+
+    try {
+        const onePost = findByUserId.findOne({ _id: req.params.id });
+        res.status(200).json(onePost);
+    } catch (error) {
+        console.warn(error)
+        res.status(404).json({ error })
+    }
+}
+
+
+function getPostByUser2(req, res, next) {
+    const sqlGetOnePost = "SELECT * FROM post JOIN users WHERE user.id=userId ORDER BY publication DESC LIMIT 50;";
+    db.exec(sqlGetOnePost, [req.body.id], function (error, result) {
+        if (error) res.status(400).json({ error });
+        res.status(200).json(result)
+    });
 }
