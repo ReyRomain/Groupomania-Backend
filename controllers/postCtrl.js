f47ea368155443fc1f87f86fb79a3d43d05747b6
Romain Rey <romainrey.contact@gmail.com>
2022-06-09 10:29:32 +0200

    feat: Ajout de createPost, modifyPost & deletePost

--- controllers/postCtrl.js
+++ controllers/postCtrl.js
@@ -2,6 +2,12 @@
  * @typedef  {import('express').Request}      IncomingMessage
  * @typedef  {import('express').Response}     ServerResponse
  * @typedef  {import('express').NextFunction} NextFunction
+ * 
+ * @typedef  {import("multer").DiskStorageOptions} multerImage
+ * 
+ * @typedef  {Object} postRequest
+ * @property {Object} body            récupère le corps de la requête
+ * @property {String} body.post      récupère le post dans le corps de la requête
 */
 
 const {db} = require("../models/database");
@@ -64,4 +70,63 @@ function getPostByUser2(req, res, next) {
         if (error) res.status(400).json({ error });
         res.status(200).json(result)
     });
+}
+
+
+
+
+/**
+ * Création d'un post
+ *
+ * @param   {IncomingMessage & multerImage & postRequest}   req    la requête complétée par une image Multer
+ * @param   {ServerResponse}                                res    la réponse
+ * @param   {NextFunction}                                  next   passe à la fonction suivante
+ * 
+ * @return  {void}                                                 envoie une réponse
+ */
+ function createPost(req, res, next) {
+    const postObject = JSON.parse(req.body.post);
+    delete postObject._id;
+    const post = new posts({
+        ...postObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    post.save()
+        .then(() => res.status(201).json({ message: 'Le post a bien été ajouté.' }))
+        .catch(error => res.status(400).json({ error }));
+}
+
+/**
+ * Modification d'une sauce
+ *
+ * @param   {IncomingMessage & multerImage & postRequest}   req    la requête complétée par une image Multer
+ * @param   {ServerResponse}                                res    la réponse
+ * @param   {NextFunction}                                  next   passe à la fonction suivante
+ *
+ * @return  {void}                                                 envoie une réponse
+ */
+ function modifyPost(req, res, next) {
+    const postObject = req.file ?
+        {
+            ...JSON.parse(req.body.post),
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+    posts.updateOne({ _id: req.params.id }, { ...postObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Le post a été modifié !' }))
+        .catch(error => res.status(400).json({ error }));
+}
+
+/**
+ * Supprime un post
+ * 
+ * @param   {IncomingMessage}   req    la requête complétée
+ * @param   {ServerResponse}    res    la réponse
+ * @param   {NextFunction}      next   passe à la fonction suivante
+ *
+ * @return  {void}                     envoie une réponse
+ */
+function deletePost(req, res, next) {
+    posts.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Le post a été supprimé' }))
+        .catch(error => res.status(400).json({ error }));
 }
