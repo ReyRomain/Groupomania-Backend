4f351bcb59913ca0b6be69e223366ed2a7e949ae
Romain Rey <romainrey.contact@gmail.com>
2022-06-11 00:17:18 +0200

    fix: Modification de getAllPosts & createPost
    Ajout de allPosts & create

--- controllers/postCtrl.js
+++ controllers/postCtrl.js
@@ -7,14 +7,16 @@
  * 
  * @typedef  {Object} postRequest
  * @property {Object} body            récupère le corps de la requête
- * @property {String} body.post      récupère le post dans le corps de la requête
+ * @property {String} body.post       récupère le post dans le corps de la requête
 */
 
 const {db} = require("../models/database");
-const {findByUserId} = require("../models/postsModel")
+const {allPosts, create, findByUserId} = require("../models/postsModel")
+
+const fs = require('fs');
 
 /**
- * [getAllPosts description]
+ * Récupération des posts
  *
  * @param   {IncomingMessage}  req   [req description]
  * @param   {ServerResponse}   res   [res description]
@@ -24,56 +26,13 @@ const {findByUserId} = require("../models/postsModel")
  */
 function getAllPosts(req, res, next) {
     try {
-        const posts = findByUserId.find();
+        const posts = allPosts();
         res.status(200).json(posts)
     } catch (error) {
         console.warn(error)
         res.status(400).json({error})
     }
 }
-
-function getAllPosts2(req, res, next) {
-    const sqlGetAllPosts = "SELECT * FROM posts JOIN users WHERE user.id=userId ORDER BY publication DESC LIMIT 50;";
-    db.exec(sqlGetAllPosts, function (error, result){
-        if (error) res.status(400).json({ error });
-        res.status(200).json(result)
-    });
-}
-
-
-
-
-/**
- * Récupération d'un post
- *
- * @param   {IncomingMessage}   req    la requête complétée
- * @param   {ServerResponse}    res    la réponse
- * @param   {NextFunction}      next   passe à la fonction suivante
- *
- * @return  {void}                     retourne le post selectionné & envoie une réponse
- */
-function getPostByUser (req, res, next) {
-
-    try {
-        const onePost = findByUserId.findOne({ _id: req.params.id });
-        res.status(200).json(onePost);
-    } catch (error) {
-        console.warn(error)
-        res.status(404).json({ error })
-    }
-}
-
-
-function getPostByUser2(req, res, next) {
-    const sqlGetOnePost = "SELECT * FROM post JOIN users WHERE user.id=userId ORDER BY publication DESC LIMIT 50;";
-    db.exec(sqlGetOnePost, [req.body.id], function (error, result) {
-        if (error) res.status(400).json({ error });
-        res.status(200).json(result)
-    });
-}
-
-
-
 
 /**
  * Création d'un post
@@ -84,10 +43,10 @@ function getPostByUser2(req, res, next) {
  * 
  * @return  {void}                                                 envoie une réponse
  */
- function createPost(req, res, next) {
+function createPost(req, res, next) {
     const postObject = JSON.parse(req.body.post);
     delete postObject._id;
-    const post = new posts({
+    const post = create({
         ...postObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
@@ -97,7 +56,7 @@ function getPostByUser2(req, res, next) {
 }
 
 /**
- * Modification d'une sauce
+ * Modification d'un post
  *
  * @param   {IncomingMessage & multerImage & postRequest}   req    la requête complétée par une image Multer
  * @param   {ServerResponse}                                res    la réponse
@@ -105,7 +64,7 @@ function getPostByUser2(req, res, next) {
  *
  * @return  {void}                                                 envoie une réponse
  */
- function modifyPost(req, res, next) {
+function modifyPost(req, res, next) {
     const postObject = req.file ?
         {
             ...JSON.parse(req.body.post),
@@ -129,4 +88,11 @@ function deletePost(req, res, next) {
     posts.deleteOne({ _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Le post a été supprimé' }))
         .catch(error => res.status(400).json({ error }));
+}
+
+module.exports = {
+    createPost,
+    deletePost,
+    getAllPosts,
+    modifyPost
 }
