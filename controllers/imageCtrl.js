d53347aeabbd21c31f253d8c99bc08bc60fbcdc5
Romain Rey <romainrey.contact@gmail.com>
2022-06-21 09:31:54 +0200

    feat: Ajout de la fonction updateImage

---
+++ controllers/imageCtrl.js
@@ -0,0 +1,14 @@
+function updateImage(req, res, next) {
+    try {
+        const thingObject = JSON.parse(req.body.user && req.body.post);
+        delete thingObject._id;
+        const thing = {
+            ...thingObject,
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        };
+        thing.save()
+        res.status(201).json({ message: 'Image ajoutée.' })
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+}
