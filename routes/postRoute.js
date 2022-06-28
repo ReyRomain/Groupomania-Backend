fff4e9785c9165e897297091e217dbcf63e9d903
Romain Rey <romainrey.contact@gmail.com>
2022-06-28 18:38:24 +0200

    feat: Ajout des routes getAllLikes & updateLikes

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -16,8 +16,10 @@ const multer = require("../middlewares/multer-config");
 const router = require('express').Router();
 
 router.get("/getAllPosts", auth, postCtrl.getAllPosts);
+router.get("/post/:id", auth, postCtrl.getAllLikes);
 router.post("/createPost", auth, multer, postCtrl.createPost);
 router.put("/modifyPost", auth, multer, postCtrl.modifyPost);
+router.put("/post/:id/updateLikes", auth, postCtrl.updateLikes);
 router.delete("/deletePost", auth, postCtrl.deletePost);
 
 module.exports = router;
